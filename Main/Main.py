9797c39e745010c628f6d720e57c6773a22a26a4
Arbow404 <71306038+Arbow404@users.noreply.github.com>
2020-10-08 18:55:55 +0700

    Menggabungkan String dengan Menggunakan Python

--- Main/Main.py
+++ Main/Main.py
@@ -1,1 +1,24 @@
 
+Print("Hello World")
+
+#1. Buat string dengan kata-kata yang dipisahkan oleh ',' dan
+# ganti koma dengan spasi
+nama = "Denyse" + "," + "Marie" + "," +"Smith"+ "," + "21" \
+       + "," + "London" + "," + "UK"
+print(nama)
+print("Denyse","Marie","Smith","21","London","Uk", sep=" ")
+print("_________________________________")
+
+#2.Membuat program untuk meminta dua string dan gabungkan keduanya, dengan spasi di antara keduanya
+# dan simpan ke dalam variabel baru yang disebut string_baru.
+nama1 = "Arbow"
+nama2 = "Velonica"
+string_baru = nama1+" "+nama2
+print(string_baru)
+
+new_string = "Arbow velonica" #new string_baru
+print(len(new_string)) #Panjang string_baru
+print(new_string.upper()) #mengubah konten menjadi huruf besar
+print("_________________________________")
+
+
